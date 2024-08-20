4bb7eca40c0c28223b7f3b82c8cb928bd2c609a6
Oliver Baumann <oliver.baumann@uni-bayreuth.de>
2024-08-20 12:23:33 +0200

    (style): Make flake8 happy (and cheat)
    Add `noqa` for bare excepts, as exception handling is a bit unclear
    right now, in terms of what types might be put into the respective
    cleaners, and what exceptions may arise.
    
    Run most of the code through a formatter, to appease line-length,
    quotes, and other stylistic warnings.

--- src/Excel2Json/Excel2Json.py
+++ src/Excel2Json/Excel2Json.py
@@ -4,7 +4,6 @@ Created on Wed Dec 13 15:35:34 2023
 
 @author: AfricaMultiple
 """
-# Libraries
 
 import re
 from datetime import datetime
@@ -32,7 +31,13 @@ class ExportJson(object):
     )
 
     # File can be path string or the dataframe object
-    def __init__(self, file: str | Path | pd.DataFrame, project_id, dspace_id, mongo_client: MongoClient | None = None):
+    def __init__(
+        self,
+        file: str | Path | pd.DataFrame,
+        project_id,
+        dspace_id,
+        mongo_client: MongoClient | None = None,
+    ):
         # Input given is project ID, default DSpace instance
         if isinstance(file, str) or isinstance(file, Path):
             self.data = MDES_CleanUp(file, mongo_client=mongo_client).execute()
@@ -40,7 +45,7 @@ class ExportJson(object):
             self.data = file
         self.project_Id = project_id
         self.dspace_Id = dspace_id
-        self.delimiter = ';'
+        self.delimiter = ";"
         self.msg = Printer()
         self.loc_client = LocClient()
 
@@ -52,84 +57,97 @@ class ExportJson(object):
 
                 data_dict = {
                     # Internal ID (Format 3-Letter Project ID - DSpace Instance/ Storage Location - Index ID)
-                    'dre_id': self.project_Id + "-" + self.set_default(value=row.user_rights, exception="No Raw Data",
-                                                                    default_value='99',
-                                                                    return_value=self.dspace_Id) + "-" + format(i,
-                                                                                                                '04x'),
+                    "dre_id": self.project_Id
+                    + "-"
+                    + self.set_default(
+                        value=row.user_rights,
+                        exception="No Raw Data",
+                        default_value="99",
+                        return_value=self.dspace_Id,
+                    )
+                    + "-"
+                    + format(i, "04x"),
                     # Bitstream
-                    'bitstream': self.set_default(
-                        value=row.user_rights, exception="No Raw Data",
+                    "bitstream": self.set_default(
+                        value=row.user_rights,
+                        exception="No Raw Data",
                         default_value="",
-                        return_value=row['filename']
+                        return_value=row["filename"],
                     ),
                     # Security Level
-                    'security': row['security_level'],
+                    "security": row["security_level"],
                     # Collection
-                    #'isCollection': self.boolclean(row['collection']),
-                    'collection': self.list_cleanup(row['collection'], []),
+                    # 'isCollection': self.boolclean(row['collection']),
+                    "collection": self.list_cleanup(row["collection"], []),
                     # Sponsor
-                    'sponsor': self.list_cleanup(row['sponsor'], []),
+                    "sponsor": self.list_cleanup(row["sponsor"], []),
                     # Project
-                    'project': {'id': row.project_id, 'name': row.project_name},
+                    "project": {"id": row.project_id, "name": row.project_name},
                     # Citation
-                    'citation': self.list_cleanup(row['citation'], []),
+                    "citation": self.list_cleanup(row["citation"], []),
                     # Url
-                    'url': self.list_cleanup(row['url'], [])
+                    "url": self.list_cleanup(row["url"], []),
                 }
 
                 # Title(s)
 
                 title_list = []
-                all_title = row.filter(regex='title').copy()
+                all_title = row.filter(regex="title").copy()
 
-                for tType in ['main'] + list(range(2, 6)):
-                    title = all_title.filter(regex=f'_{tType}')
-                    title.index = [c.replace(f'_{tType}', '') for c in title.index]
-                    if not pd.isna(title['title']):
-                        title['title'] = str(title['title']).strip()
+                for tType in ["main"] + list(range(2, 6)):
+                    title = all_title.filter(regex=f"_{tType}")
+                    title.index = [c.replace(f"_{tType}", "") for c in title.index]
+                    if not pd.isna(title["title"]):
+                        title["title"] = str(title["title"]).strip()
                         title_list.append(title.to_dict())
                     else:
                         continue
-                data_dict['titleInfo'] = title_list
+                data_dict["titleInfo"] = title_list
 
                 # Dates
 
                 date_dict = {}
-                all_date = row.filter(regex='date_').copy()
+                all_date = row.filter(regex="date_").copy()
 
                 for k in all_date.keys():
                     if not pd.isna(row[k]):
-                        date_dict[k.replace('date_', '')] = self.datewrangling(all_date[k])
+                        date_dict[k.replace("date_", "")] = self.datewrangling(
+                            all_date[k]
+                        )
                     else:
                         continue
 
-                data_dict['dateInfo'] = date_dict
+                data_dict["dateInfo"] = date_dict
 
                 # Roles
 
                 role_list = []
-                all_role = row.filter(regex=r'name_\d+$|role_\d+$|affl_\d+$')
+                all_role = row.filter(regex=r"name_\d+$|role_\d+$|affl_\d+$")
 
-                for rtype in range(1, int(len(all_role.index)/3)+1):
-                    role = all_role.filter(regex=f'_{rtype}$')
-                    role.index = [c.replace(f'_{rtype}', '') for c in role.index]
+                for rtype in range(1, int(len(all_role.index) / 3) + 1):
+                    role = all_role.filter(regex=f"_{rtype}$")
+                    role.index = [c.replace(f"_{rtype}", "") for c in role.index]
                     built = self.build_role(role)
                     if built is None:
                         continue
 
                     role_list.append(built)
 
-                data_dict['name'] = role_list
+                data_dict["name"] = role_list
 
                 # Note
-
-                data_dict['note'] = row['note']
+                data_dict["note"] = row["note"]
 
                 # Subject
 
-                subjects = self.list_cleanup(row['subject'], [])
+                subjects = self.list_cleanup(row["subject"], [])
                 data_dict["subject"] = [
-                    {"uri": np.nan, "authority": np.nan, "origLabel": s, "authLabel": np.nan}
+                    {
+                        "uri": np.nan,
+                        "authority": np.nan,
+                        "origLabel": s,
+                        "authLabel": np.nan,
+                    }
                     for s in subjects
                 ]
 
@@ -137,95 +155,107 @@ class ExportJson(object):
 
                 rel_items = {}
 
-                for c in row.filter(like='rel_').index:
+                for c in row.filter(like="rel_").index:
                     if not pd.isna(row[c]):
                         rel_items[c] = self.list_cleanup(row[c], [])
 
-                data_dict['relatedItems'] = rel_items
+                data_dict["relatedItems"] = rel_items
 
                 # Identifiers
 
                 identifiers = []
-                ids = row.filter(regex=r'identifier_\d+$|identifier_type_\d+$')
+                ids = row.filter(regex=r"identifier_\d+$|identifier_type_\d+$")
 
-                for n in range(1, int(len(ids.index)/2)*1):
-                    id_n = ids.filter(regex=f'identifier_{n}$|identifier_type_{n}$')
-                    id_n.index = [c.replace(f'_{n}', '') for c in id_n.index]
-                    if not pd.isna(id_n['identifier']):
+                for n in range(1, int(len(ids.index) / 2) * 1):
+                    id_n = ids.filter(regex=f"identifier_{n}$|identifier_type_{n}$")
+                    id_n.index = [c.replace(f"_{n}", "") for c in id_n.index]
+                    if not pd.isna(id_n["identifier"]):
                         identifiers.append(id_n.to_dict())
                     else:
                         continue
 
-                data_dict['identifier'] = identifiers
+                data_dict["identifier"] = identifiers
 
                 # Location
 
                 # Origin
                 origin_list = []
 
-                origin = row.filter(like='loc_')
-                origin.index = [col.replace('loc_origin_', '') for col in origin.index]
+                origin = row.filter(like="loc_")
+                origin.index = [col.replace("loc_origin_", "") for col in origin.index]
                 for f in origin.index:
                     try:
-                        origin[f] = list(map(lambda x: x.replace('99', ""), self.list_cleanup(origin[f], [])))
+                        origin[f] = list(
+                            map(
+                                lambda x: x.replace("99", ""),
+                                self.list_cleanup(origin[f], []),
+                            )
+                        )
                     except TypeError:
                         pass
-                if range(len(origin['l1'])) == 1:
+                if range(len(origin["l1"])) == 1:
                     origin_list = [origin.to_dict()]
                 else:
-                    for n in range(len(origin['l1'])):
-                        origin_dict = {"l1": origin['l1'][n], # country
-                                    "l2": origin['l2'][n] if len(origin['l2']) >= n + 1 and origin['l2'][n] != "" else np.nan, # region
-                                    "l3": origin['l3'][n] if len(origin['l3']) >= n + 1 and origin['l3'][n] != "" else np.nan} # subregion
+                    for n in range(len(origin["l1"])):
+                        origin_dict = {
+                            "l1": origin["l1"][n],  # country
+                            "l2": (
+                                origin["l2"][n]
+                                if len(origin["l2"]) >= n + 1 and origin["l2"][n] != ""
+                                else np.nan
+                            ),  # region
+                            "l3": (
+                                origin["l3"][n]
+                                if len(origin["l3"]) >= n + 1 and origin["l3"][n] != ""
+                                else np.nan
+                            ),
+                        }  # subregion
                         origin_list.append(origin_dict)
 
                 # Current Location
 
-                current_loc = self.list_cleanup(row['current_location'], [])
+                current_loc = self.list_cleanup(row["current_location"], [])
 
-                data_dict['location'] = {'origin': origin_list, 'current': current_loc}
+                data_dict["location"] = {"origin": origin_list, "current": current_loc}
 
                 # Access Condition
 
                 # Usage and Distribution License
-
-                rights = self.list_cleanup(row['use_copy_rights'], [])
+                rights = self.list_cleanup(row["use_copy_rights"], [])
 
                 # Repository Access Specification
 
                 use = {}
-                user = row.filter(like='user')
-                use['type'] = user.user_rights
-                use['***REMOVED***s'] = self.list_cleanup(user.users, 'All')
+                user = row.filter(like="user")
+                use["type"] = user.user_rights
+                use["***REMOVED***s"] = self.list_cleanup(user.users, "All")
 
-                data_dict['accessCondition'] = {'rights': rights, 'usage': use}
+                data_dict["accessCondition"] = {"rights": rights, "usage": use}
 
                 # Resource Type
-
-                data_dict['typeOfResource'] = row['resource_type']
+                data_dict["typeOfResource"] = row["resource_type"]
 
                 # Genre
 
                 genre_type = {}
 
-                genre = row.filter(like='genre')
-                genre.index = [col.replace('genre_', '') for col in genre.index]
+                genre = row.filter(like="genre")
+                genre.index = [col.replace("genre_", "") for col in genre.index]
                 for g in genre.index:
                     if not pd.isna(genre[g]):
                         genre_type[g] = self.list_cleanup(genre[g], [])
                     else:
                         continue
 
-                data_dict['genre'] = genre_type
+                data_dict["genre"] = genre_type
 
                 # Language
-
-                data_dict['language'] = self.list_cleanup(row.language, [])
+                data_dict["language"] = self.list_cleanup(row.language, [])
 
                 # Physical Description
                 physical_desc = {}
-                physical = row.filter(regex='physical_|dig_')
-                physical.index = [re.sub(r'\w+_', '', col) for col in physical.index]
+                physical = row.filter(regex="physical_|dig_")
+                physical.index = [re.sub(r"\w+_", "", col) for col in physical.index]
                 i = 0
                 for key in physical.index:
                     i += 1
@@ -234,23 +264,19 @@ class ExportJson(object):
                     else:
                         physical_desc[key] = self.list_cleanup(physical[key], [])
 
-                data_dict['physicalDescription'] = physical_desc
+                data_dict["physicalDescription"] = physical_desc
 
                 # Abstract
-
-                data_dict['abstract'] = row.abstract
+                data_dict["abstract"] = row.abstract
 
                 # Table of Content
-
-                data_dict['tableOfContents'] = row['table_contents']
+                data_dict["tableOfContents"] = row["table_contents"]
 
                 # Target Audience
-
-                data_dict['targetAudience'] = self.list_cleanup(row['target_aud'], [])
+                data_dict["targetAudience"] = self.list_cleanup(row["target_aud"], [])
 
                 # Tags
-
-                data_dict['tags'] = self.list_cleanup(row['tags'], [])
+                data_dict["tags"] = self.list_cleanup(row["tags"], [])
 
                 # Append to dictionary list
                 json_list.append(data_dict)
@@ -259,14 +285,16 @@ class ExportJson(object):
 
         self.msg.info("Fetching LoC URIs for subject headings")
         for item in json_list:
-            for s in item['subject']:
-                res = self.loc_client.get_subject(s['origLabel'])
+            for s in item["subject"]:
+                res = self.loc_client.get_subject(s["origLabel"])
                 if res is None:
                     continue
                 label, uri = res
-                s['authLabel'] = label
-                s['uri'] = uri
-                s['authority'] = "http://id.loc.gov/authorities/subjects/collection_LCSHAuthorizedHeadings"
+                s["authLabel"] = label
+                s["uri"] = uri
+                s["authority"] = (
+                    "http://id.loc.gov/authorities/subjects/collection_LCSHAuthorizedHeadings"
+                )
 
         self.msg.good("LoC fetch completed!")
 
@@ -275,12 +303,17 @@ class ExportJson(object):
     # Auxiliary Functions
     def list_cleanup(self, list_string, exception):
         try:
-            return list(filter(None, [l.strip() for l in list_string.split(self.delimiter)]))
-        except:
+            return list(
+                filter(
+                    None, [part.strip() for part in list_string.split(self.delimiter)]
+                )
+            )
+        except:  # noqa: E722
             pass
+
         try:
             return list(filter(None, list_string))
-        except:
+        except:  # noqa: E722
             return exception
 
     def set_default(self, value, exception, default_value, return_value):
@@ -300,25 +333,25 @@ class ExportJson(object):
                 return datetime.strptime(date, "%Y%m").isoformat()
             elif date_length == 4:
                 return datetime.strptime(date, "%Y").isoformat()
-        except:
+        except:  # noqa: E722
             return np.nan
 
     # Date Cleanup Function
     def datewrangling(self, date_string):
         try:
-            date = str.strip(date_string.replace(';', ''))
-        except:
+            date = date_string.replace(";", "").strip()
+        except:  # noqa: E722
             date = date_string
-        if len(str(date).split('--')) == 2:
-            date_start = self.date_convert(str(date).split('--')[0])
-            date_end = self.date_convert(str(date).split('--')[1])
+        if len(str(date).split("--")) == 2:
+            date_start = self.date_convert(str(date).split("--")[0])
+            date_end = self.date_convert(str(date).split("--")[1])
         else:
             date_start = np.nan
             try:
                 date_end = self.date_convert(date)
-            except:
+            except:  # noqa
                 date_end = np.nan
-        return {'start': date_start, 'end': date_end}
+        return {"start": date_start, "end": date_end}
 
     # Boolean Handling
 
@@ -330,7 +363,6 @@ class ExportJson(object):
         else:
             return None
 
-
     @classmethod
     def build_role(cls, data: dict) -> Optional[collection.Role]:
         if pd.isna(data["name"]):
@@ -338,20 +370,22 @@ class ExportJson(object):
 
         # set the 'person'-qualifier as default; will be overwritten if another
         # qualifier is found
-        name = collection.Name(label=data["name"].strip(), qualifier=Qualifiers.PERSON.value)
+        name = collection.Name(
+            label=data["name"].strip(), qualifier=Qualifiers.PERSON.value
+        )
         role = collection.Role(name=name, affl=[], role="")
 
         if "role" in data and not pd.isna(data["role"]):
             role["role"] = data["role"].strip()
 
-        match = cls.qualifiers_pattern.match(role['name']['label'])
+        match = cls.qualifiers_pattern.match(role["name"]["label"])
         if match:
             role["name"]["label"] = match.group("name")
             role["name"]["qualifier"] = match.group("qualifier")
 
-        if pd.isna(data['affl']):
+        if pd.isna(data["affl"]):
             return role
 
-        role['affl'] = [a.strip() for a in data['affl'].split(';') if a.split()]
+        role["affl"] = [a.strip() for a in data["affl"].split(";") if a.split()]
 
         return role
